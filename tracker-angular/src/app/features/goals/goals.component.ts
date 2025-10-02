b8e04a46da50c8437f4bb488974f09bd2cca35f2
samir <samir.ad19ti@gmail.com>
2025-10-02 21:36:44 +0100

    implement the fuctionality of  features goalss angular

--- tracker-angular/src/app/features/goals/goals.component.ts
+++ tracker-angular/src/app/features/goals/goals.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { GoalsCardComponent } from '../../shared/components/goals-card/goals-card.component'; 
+import { DailyGoalProgressComponent } from '../../shared/components/daily-goal-progress/daily-goal-progress.component'; 
+import { LayoutComponent } from '../../shared/components/layout/layout.component'; 
 @Component({
   selector: 'app-goals',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    GoalsCardComponent, 
+    DailyGoalProgressComponent,
+    LayoutComponent
+  ],
   templateUrl: './goals.component.html',
   styleUrl: './goals.component.scss'
 })
