cad2c094bae6089c2b5906a86dbda59dbcc1f261
samir <samir.ad19ti@gmail.com>
2025-10-02 20:26:17 +0100

    create sleeps  page

--- tracker-react/src/pages/Sleep.jsx
+++ tracker-react/src/pages/Sleep.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import SleepCard from '../components/SleepCard'
+import WeeklySleepChart from '../components/charts/WeeklySleepChart'
+import Layout from '../components/Layout'
 
 const Sleep = () => {
   return (
-    <div>Sleep</div>
+      <Layout>
+        <div className="space-y-6">
+          <SleepCard />
+          <WeeklySleepChart />
+        </div>
+      </Layout>
   )
 }
 
