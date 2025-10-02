467fc87b313007027fc61819f7d579fe6748783b
samir <samir.ad19ti@gmail.com>
2025-10-02 21:45:30 +0100

    implement the fuctionality of  features dashboard angular

--- tracker-angular/src/app/features/dashboard/dashboard.component.ts
+++ tracker-angular/src/app/features/dashboard/dashboard.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common'; 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { WaterCardComponent } from '../../shared/components/water-card/water-card.component'; 
+import { SleepCardComponent } from '../../shared/components/sleep-card/sleep-card.component'; 
+import { StepsCardComponent } from '../../shared/components/steps-card/steps-card.component'; 
+import { MoodsCardComponent } from '../../shared/components/moods-card/moods-card.component'; 
+import { MealsCardComponent } from '../../shared/components/meals-card/meals-card.component'; 
+import { ExercicesCardComponent } from '../../shared/components/exercices-card/exercices-card.component'; 
+import { LayoutComponent } 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule,
+     MatGridListModule,
+     WaterCardComponent,
+     SleepCardComponent,
+     StepsCardComponent,
+     MoodsCardComponent,
+     MealsCardComponent,
+     ExercicesCardComponent,
+     LayoutComponent], 
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
-
+  isSmallScreen() {
+    return window.innerWidth < 768;
+  }
 }
