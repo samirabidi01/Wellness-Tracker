b69ddf5c63ececb240daa0f0c833538fc21c80f5
samir <samir.ad19ti@gmail.com>
2025-10-02 20:29:12 +0100

    create  the navigation of routes for the web applicatio

--- tracker-react/src/App.jsx
+++ tracker-react/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Dashboard from './pages/Dashboard';
+import {Route, Routes } from 'react-router-dom';
+import Moods from './pages/Moods';
+import Sleep from './pages/Sleep';
+import Steps from './pages/Steps';
+import Water from './pages/Water';
+import Meals from './pages/Meals';
+import Exercices from './pages/Exercices';
+import Goals from './pages/Goals';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
+    <Routes>
+      <Route path='/' element={<Dashboard />}></Route>
+      <Route path='/mood' element={<Moods />}></Route>
+      <Route path='/sleep' element={<Sleep />}></Route>
+      <Route path='/step' element={<Steps />}></Route>
+      <Route path='/water' element={<Water/>}></Route>
+      <Route path='/meal' element={<Meals/>}></Route>
+      <Route path='/exercise' element={<Exercices />}></Route>
+      <Route path='/goal' element={<Goals />}></Route>
+    </Routes>
+  );
+};
 export default App
