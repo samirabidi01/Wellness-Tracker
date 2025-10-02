b34b5afb75de2034a4b4208daa68b1f29ee72e1f
samir <samir.ad19ti@gmail.com>
2025-10-02 21:33:20 +0100

    implement the fuctionality of  features sleep angular

--- tracker-angular/src/app/features/sleep/sleep.component.ts
+++ tracker-angular/src/app/features/sleep/sleep.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SleepCardComponent } from '../../shared/components/sleep-card/sleep-card.component'; 
+import { WeeklySleepChartComponent } from '../../shared/charts/weekly-sleep-chart/weekly-sleep-chart.component'; 
+import { LayoutComponent } from '../../shared/components/layout/layout.component'; 
 
 @Component({
   selector: 'app-sleep',
-  imports: [],
+  standalone: true,
   templateUrl: './sleep.component.html',
-  styleUrl: './sleep.component.scss'
+  styleUrls: ['./sleep.component.scss'],
+  imports: [
+    CommonModule,
+    LayoutComponent,
+    SleepCardComponent,
+    WeeklySleepChartComponent
+  ]
 })
-export class SleepComponent {
-
-}
+export class SleepComponent {}
