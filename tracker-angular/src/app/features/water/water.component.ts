b5aa01bee22f7f376d55bf03043341e76f156f2f
samir <samir.ad19ti@gmail.com>
2025-10-02 21:30:19 +0100

    implement the fuctionality of  features water angular

--- tracker-angular/src/app/features/water/water.component.ts
+++ tracker-angular/src/app/features/water/water.component.ts
@@ -1,11 +1,14 @@
+// water.component.ts
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { WeeklyWaterChartComponent } from '../../shared/charts/weekly-water-chart/weekly-water-chart.component'; 
+import { LayoutComponent } from '../../shared/components/layout/layout.component';
+import { WaterCardComponent } from '../../shared/components/water-card/water-card.component';
 @Component({
   selector: 'app-water',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule,LayoutComponent,WaterCardComponent,WeeklyWaterChartComponent], 
   templateUrl: './water.component.html',
-  styleUrl: './water.component.scss'
+  styleUrls: ['./water.component.scss']
 })
-export class WaterComponent {
-
-}
+export class WaterComponent {}
