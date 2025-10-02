e73b0fdf4ad1416a824b40edb976b75b96c19fb5
samir <samir.ad19ti@gmail.com>
2025-10-02 21:26:18 +0100

    implement the fuctionality of component sleep chart angular

--- tracker-angular/src/app/shared/charts/weekly-steps-chart/weekly-steps-chart.component.ts
+++ tracker-angular/src/app/shared/charts/weekly-steps-chart/weekly-steps-chart.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 
 @Component({
   selector: 'app-weekly-steps-chart',
-  imports: [],
+  imports: [ CanvasJSAngularChartsModule],
   templateUrl: './weekly-steps-chart.component.html',
-  styleUrl: './weekly-steps-chart.component.scss'
+  styleUrls: ['./weekly-steps-chart.component.scss']
 })
 export class WeeklyStepsChartComponent {
-
+  chartOptions = {
+    title: {
+      text: "Weekly Steps"
+    },
+    theme: "light2",
+    animationEnabled: false,  
+    exportEnabled: true,      
+    axisY: {
+      includeZero: true,
+      valueFormatString: "#,##0"
+    },
+    data: [{
+      type: "line",  // change to "column", "area", "pie", etc.
+      color: "#10b981",
+      markerSize: 8,
+      dataPoints: [
+        { label: "Mon", y: 8500 },
+        { label: "Tue", y: 12000 },
+        { label: "Wed", y: 9500 },
+        { label: "Thu", y: 11000 },
+        { label: "Fri", y: 7000 },
+        { label: "Sat", y: 14000 },
+        { label: "Sun", y: 10000 }
+      ]
+    }]
+  };
 }
