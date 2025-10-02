54b35f8db565bfd3597d5dee904421c05aa0bf86
samir <samir.ad19ti@gmail.com>
2025-10-02 21:19:48 +0100

    update of component mealscards angular

--- tracker-angular/src/app/shared/components/meals-card/meals-card.component.ts
+++ tracker-angular/src/app/shared/components/meals-card/meals-card.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 @Component({
   selector: 'app-meals-card',
-  imports: [],
   templateUrl: './meals-card.component.html',
-  styleUrl: './meals-card.component.scss'
+  styleUrls: ['./meals-card.component.scss'],
+  standalone: true,
+  imports: [CommonModule,CommonModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule]
 })
-export class MealsCardComponent {
+export class MealsCardComponent implements OnInit, OnDestroy {
+  defaultMeals = {
+    breakfast: '',
+    lunch: '',
+    dinner: '',
+    snacks: '',
+  };
 
+  meals: { [key: string]: string } = { ...this.defaultMeals };
+  storageListener: any;
+
+  ngOnInit(): void {
+    this.loadMeals();
+
+    this.storageListener = () => this.loadMeals();
+    window.addEventListener('storage', this.storageListener);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('storage', this.storageListener);
+  }
+
+  loadMeals(): void {
+    const stored = localStorage.getItem('meals');
+    this.meals = stored ? JSON.parse(stored) : { ...this.defaultMeals };
+  }
 }
