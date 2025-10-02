ebd1fa6f3b7c8f43741d8e5b0ea65e8c027e7aa6
samir <samir.ad19ti@gmail.com>
2025-10-02 21:38:26 +0100

    implement the fuctionality of  features exercices angular

--- tracker-angular/src/app/features/exercices/exercices.component.ts
+++ tracker-angular/src/app/features/exercices/exercices.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from '../../shared/components/layout/layout.component'; 
+import { ExercicesCardComponent } from '../../shared/components/exercices-card/exercices-card.component'; 
+import { WelnessComponent } from '../../shared/components/welness/welness.component'; 
 @Component({
   selector: 'app-exercices',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ExercicesCardComponent,
+    WelnessComponent,
+    LayoutComponent, 
+ 
+    
+  ],
   templateUrl: './exercices.component.html',
   styleUrl: './exercices.component.scss'
 })
