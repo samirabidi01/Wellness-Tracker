ed7b1c9152fa99011d311dc24b005384de238c78
samir <samir.ad19ti@gmail.com>
2025-10-02 21:31:49 +0100

    implement the fuctionality of  features steps angular

--- tracker-angular/src/app/features/steps/steps.component.ts
+++ tracker-angular/src/app/features/steps/steps.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { StepsCardComponent } from '../../shared/components/steps-card/steps-card.component'; 
+import { LayoutComponent } from '../../shared/components/layout/layout.component'; 
+import { WeeklyStepsChartComponent } from '../../shared/charts/weekly-steps-chart/weekly-steps-chart.component'; 
 @Component({
   selector: 'app-steps',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule,StepsCardComponent,WeeklyStepsChartComponent,LayoutComponent],
   templateUrl: './steps.component.html',
   styleUrl: './steps.component.scss'
 })
