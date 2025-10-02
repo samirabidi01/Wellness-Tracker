d3ff5d06e0dcf39f817f465fedeb6a212865e8d6
samir <samir.ad19ti@gmail.com>
2025-10-02 21:25:31 +0100

    implement the fuctionality of component water chart angular

--- tracker-angular/src/app/shared/charts/weekly-water-chart/weekly-water-chart.component.ts
+++ tracker-angular/src/app/shared/charts/weekly-water-chart/weekly-water-chart.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 
 @Component({
   selector: 'app-weekly-water-chart',
-  imports: [],
+  imports:[CanvasJSAngularChartsModule],
   templateUrl: './weekly-water-chart.component.html',
-  styleUrl: './weekly-water-chart.component.scss'
+  styleUrls: ['./weekly-water-chart.component.scss']
 })
 export class WeeklyWaterChartComponent {
+  dailyGoal = 8;
 
+  chartOptions = {
+    title: {
+      text: "Weekly Water Intake"
+    },
+    theme: "light2",
+    animationEnabled: false, // ⛔ no animations
+    exportEnabled: true,
+    axisY: {
+      includeZero: true,
+      title: "Glasses",
+      interval: 2
+    },
+    data: [{
+      type: "column",   // ✅ Column chart
+      color: "#3b82f6", // Tailwind blue-500
+      yValueFormatString: "#,##0",
+      dataPoints: [
+        { label: "Mon", y: 6 },
+        { label: "Tue", y: 8 },
+        { label: "Wed", y: 5 },
+        { label: "Thu", y: 7 },
+        { label: "Fri", y: 9 },
+        { label: "Sat", y: 4 },
+        { label: "Sun", y: 10 }
+      ]
+    }]
+  };
 }
